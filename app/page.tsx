d95085b7219f1cea0dcd42a646ef403db39f7c7d
chungchihhan <harryup2000@gmail,com>
2024-03-13 17:53:32 +0800

    about section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Intro from "@/components/intro";
 import SectionDivider from "@/components/section-divider";
+import About from "@/components/about";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
       <Intro />
       <SectionDivider />
+      <About />
     </main>
   );
 }
---
+++ components/about.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React from "react";
+import SectionHeading from "@/components/section-heading";
+import { motion } from "framer-motion";
+
+export default function About() {
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        After graduating with a degree in National Taiwan University
+        <span className="font-medium">Accounting</span>, I decided to pursue my
+        passion for programming. I dedicated in a coding course and learned{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          Next.js, MERN, TailwindCSS, LlamaIndex and LangChain.
+        </span>
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy doing some
+        photography, playing basketball and watching movies. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">how to play a guitar.</span>
+      </p>
+    </motion.section>
+  );
+}
