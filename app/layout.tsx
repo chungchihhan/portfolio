e2815114112caaa293fff7ae3406f16ad90f7929
chungchihhan <harryup2000@gmail.com>
2025-02-11 14:45:27 +0800

    style: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Chung Chih-han | Personal Portfolio",
-  description:
-    "chih-han is a post-graduate student at the university of Taiwan.",
+  description: "Chung Chih-han's personal portfolio",
+  icons: {
+    icon: [
+      {
+        url: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥪</text></svg>",
+        type: "image/svg+xml",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
