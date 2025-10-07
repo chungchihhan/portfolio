a3253d764c374967048bf205a178fee1b5c0521a
chungchihhan <harryup2000@gmail.com>
2025-10-07 23:22:52 +0800

    feat: add /blogs and redirects for blogs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        // 來源：當使用者造訪 chih-han.com/blogs 時
+        source: "/blogs",
+        // 目標：重新導向到您的 HackMD 網址
+        destination: "https://hackmd.io/@chih-han",
+        // 狀態：設定為 true 會傳回 HTTP 301 狀態碼 (永久轉址)，對 SEO 最好
+        permanent: true,
+      },
+      // (可選) 處理 /blogs/ 之後的任何路徑，全部導回 HackMD 主頁
+      {
+        source: "/blogs/:path*",
+        destination: "https://hackmd.io/@chih-han",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
