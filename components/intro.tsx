dbaf05cba5db22fea50b0efd6c28ed20dbdcd273
chungchihhan <harryup2000@gmail.com>
2025-10-07 23:32:27 +0800

    fix: fix spelling

--- components/intro.tsx
+++ components/intro.tsx
@@ -68,9 +68,9 @@ export default function Intro() {
       >
         <Link
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-          href="/blogs"
+          href="/blog"
         >
-          See my Blogs{" "}
+          See my Blog{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-2 transition" />
         </Link>
         <Link
--- next.config.js
+++ next.config.js
@@ -3,16 +3,16 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        // 來源：當使用者造訪 chih-han.com/blogs 時
-        source: "/blogs",
+        // 來源：當使用者造訪 chih-han.com/blog 時
+        source: "/blog",
         // 目標：重新導向到您的 HackMD 網址
         destination: "https://hackmd.io/@chih-han",
         // 狀態：設定為 true 會傳回 HTTP 301 狀態碼 (永久轉址)，對 SEO 最好
         permanent: true,
       },
-      // (可選) 處理 /blogs/ 之後的任何路徑，全部導回 HackMD 主頁
+      // (可選) 處理 /blog/ 之後的任何路徑，全部導回 HackMD 主頁
       {
-        source: "/blogs/:path*",
+        source: "/blog/:path*",
         destination: "https://hackmd.io/@chih-han",
         permanent: true,
       },
